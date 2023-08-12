0a09adc66db0c1fb5d48c534c7b98bb4ee1f5ba1
davi-debug-blip-69 <42500271+DaviRP2018@users.noreply.github.com>
2023-08-12 20:02:03 -0300

    Ruby, metal detector

---
+++ src/main/java/davi/modid/item/ModItems.java
@@ -0,0 +1,35 @@
+package davi.modid.item;
+
+import davi.modid.Davi;
+import davi.modid.item.custom.MetalDetectorItem;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings()));
+    public static final Item RAW_RUBY = registerItem("raw_ruby", new Item(new FabricItemSettings()));
+
+    public static final Item METAL_DETECTOR = registerItem("metal_detector",
+            new MetalDetectorItem(new FabricItemSettings().maxDamage(64)));
+
+    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+        entries.add(RUBY);
+        entries.add(RAW_RUBY);
+    }
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(Davi.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        Davi.LOGGER.info("Registering Mod Items for " + Davi.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
+    }
+}
---
+++ src/main/java/davi/modid/item/custom/MetalDetectorItem.java
@@ -0,0 +1,55 @@
+package davi.modid.item.custom;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+
+
+public class MetalDetectorItem extends Item {
+    // Este item detecta metais 20 blocos abaixo dele
+    public MetalDetectorItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        if (!context.getWorld().isClient()) {
+            BlockPos positionClicked = context.getBlockPos();
+            PlayerEntity player = context.getPlayer();
+
+            for (int i = 0; i <= 20; i++) {
+                BlockPos positionClickedDown = positionClicked.down(i);
+                player.sendMessage(Text.literal(String.valueOf(positionClickedDown)), false);
+
+                BlockState state = context.getWorld().getBlockState(positionClickedDown);
+                player.sendMessage(Text.literal(String.valueOf(state)), false);
+
+                if (isMetalOreBlock(state)) {
+                    outputMetalOreDetection(player);
+                    break;
+                }
+            }
+        }
+
+        context.getStack().damage(
+            1,
+            context.getPlayer(),
+            playerEntity -> playerEntity.sendToolBreakStatus(playerEntity.getActiveHand())
+        );
+
+        return ActionResult.SUCCESS;
+    }
+
+    private void outputMetalOreDetection(PlayerEntity player) {
+        player.sendMessage(Text.literal("<beep> <beep>"), false);
+    }
+
+    private boolean isMetalOreBlock(BlockState state) {
+        return state.isOf(Blocks.IRON_ORE) || state.isOf(Blocks.COPPER_ORE) || state.isOf(Blocks.GOLD_ORE);
+    }
+}
