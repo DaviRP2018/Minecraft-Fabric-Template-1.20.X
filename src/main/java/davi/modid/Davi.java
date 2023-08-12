1c27c753209ca98f92d2b55e43a5370263e0c2af
davi-debug-blip-69 <42500271+DaviRP2018@users.noreply.github.com>
2023-08-12 17:00:21 -0300

    first commit

---
+++ src/main/java/davi/modid/Davi.java
@@ -0,0 +1,17 @@
+package davi.modid;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Davi implements ModInitializer {
+	public static final String MOD_ID = "davi";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/davi/modid/DaviClient.java
@@ -0,0 +1,11 @@
+package davi.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DaviClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
